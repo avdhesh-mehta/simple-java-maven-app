ea7b274ecd686cd092c1d1418e37ee5f88f659e9
avdhesh-mehta <39118252+avdhesh-mehta@users.noreply.github.com>
2018-05-17 13:16:18 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,3 +3,7 @@ isExistApp=`pgrep httpd`
 if [[ -n  $isExistApp ]]; then
     service httpd stop        
 fi
+isExistApp1=`pgrep tomcat8`
+if [[ -n  $isExistApp1 ]]; then
+    service tomcat8 stop        
+fi
